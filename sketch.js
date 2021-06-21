97934e14a42c5b9a68dd6a555842efc93bb7ddba
sanjay-m-suresh <86246637+sanjay-m-suresh@users.noreply.github.com>
2021-06-21 20:30:43 +0530

    Project 10 by Sanjay

--- sketch.js
+++ sketch.js
@@ -5,11 +5,29 @@ var r = 0;
 var g = 0;
 var b = 0;
 
-function setup() {
-  createCanvas(400, 400);  
+function setup() 
+{
+  
+  createCanvas(400, 400);
+  
+  btn_red = createButton("RED");
+  btn_red.position(100,50);
+  btn_red.mousePressed(red_bg);
+
+  btn_green = createButton("GREEN");
+  btn_green.position(250,50);
+  btn_green.mousePressed(green_bg);
 }
 
-function draw() {
-  background(r,g,b);
+function red_bg() 
+{
+  background('rgb(225,0,0)');
+
+ 
+}
+
+function green_bg() 
+{
+  background('rgb(0,225,0)');
 }
 
